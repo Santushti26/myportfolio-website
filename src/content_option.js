7b396b33e37ebadbb15a753bffc0b1672a7f1617
Santushti26 <santushtiranjan@gmail.com>
2024-12-09 12:45:04 +0530

    ,

---
+++ src/content_option.js
@@ -0,0 +1,176 @@
+const logotext = "SANTUSHTI";
+const meta = {
+    title: "Santushti",
+    description: "I’m Santushti, UI UX Designer and Full Stack Web Developer ,currently in Btech final Year",
+};
+
+const introdata = {
+    title: "I’m Santushti",
+    animated: {
+        first: "I love UI UX designing",
+        second: "I design cool posters",
+        third: "I am a Full Stack Web Developer",
+    },
+    description: "A passionate UI/UX designer crafting intuitive and visually appealing designs. My portfolio showcases innovative projects blending creativity and technical expertise for seamless user experiences.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I’m Santushti Ranjan, a UI/UX designer passionate about crafting intuitive, visually appealing designs. My portfolio showcases innovative projects blending creativity and technical expertise for seamless user experiences.",
+};
+const worktimeline = [{
+        jobtitle: "UI UX Internship",
+        where: "EJY HEALTH",
+        date: "2022",
+    },
+    {
+        jobtitle: "UI UX DESIGNER",
+        where: "LOCAL VILLE",
+        date: "2023",
+    },
+    {
+        jobtitle: "Freelancing since...",
+        where: "(Self)",
+        date: "2022",
+    },
+    {
+        jobtitle: "Design Head",
+        where: "Design club- pixelate",
+        date: "2022",
+    },
+    {
+        jobtitle: "Chairperson",
+        where: "Design club- pixelate",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Figma",
+        value: 90,
+    },
+    {
+        name: "Adobe Illustrator",
+        value: 85,
+    },
+    {
+        name: "HTML, CSS, JS",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Next Js",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I specialize in creating intuitive and visually captivating user interfaces that enhance user experiences. From user research and personas to wireframing, prototyping, and usability testing, I ensure every design is both functional and aesthetically appealing. My expertise in tools like Figma and Adobe XD allows me to bring concepts to life with precision and creativity.",
+    },
+    {
+        title: "Graphic Designing & Video Editing (short form)",
+        description: "I craft visually engaging content that captures attention and communicates effectively. Whether it's designing logos, posters, or social media graphics, or editing short-form videos and reels, I deliver brand-focused content tailored to diverse needs. My proficiency in tools like Adobe Photoshop, Illustrator, and Premiere Pro ensures professional quality and creativity in every project.",
+    },
+    {
+        title: "Full Stack Web Development",
+        description: "I develop responsive and dynamic web applications, ensuring a seamless user experience from front-end to back-end. With expertise in technologies like React, Node.js, and MongoDB, I build scalable and secure solutions tailored to specific requirements. My goal is to create functional, user-friendly websites that align with modern web standards.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "santushtiranjan@gmail.com",
+    YOUR_FONE: "+91 9968612220",
+    description: "I am Santushti Ranjan, a passionate UI/UX designer with a knack for creating intuitive and visually stunning designs. With a strong foundation in full-stack development, I blend creativity with technical expertise to craft seamless user experiences. My portfolio highlights innovative projects that reflect my commitment to excellence and design-driven problem-solving. Explore my journey and discover how I bring ideas to life through design! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Santushti26",
+    instagram: "https://www.instagram.com/santushti_26/",
+    linkedin: "https://www.linkedin.com/in/santushti-r-095249215/",
+    twitter: "https://x.com/Santushti_26",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};

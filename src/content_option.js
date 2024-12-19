4612a4028d006dd89291819aab517f6ca227ce8a
Santushti26 <santushtiranjan@gmail.com>
2024-12-20 03:49:40 +0530

    .

--- src/content_option.js
+++ src/content_option.js
@@ -83,69 +83,93 @@ const services = [{
         description: "I develop responsive and dynamic web applications, ensuring a seamless user experience from front-end to back-end. With expertise in technologies like React, Node.js, and MongoDB, I build scalable and secure solutions tailored to specific requirements. My goal is to create functional, user-friendly websites that align with modern web standards.",
     },
 ];
-import profileImage1 from "./assets/images/Frame 11.png";
+import profileImage1 from "./assets/images/p1.png";
+import img2 from "./assets/images/p2.png";
+import img3 from "./assets/images/p3.png";
+import img4 from "./assets/images/p4.png";
+import img5 from "./assets/images/p5.png";
+import img6 from "./assets/images/p6.png";
+import img7 from "./assets/images/p1.png";
+import img8 from "./assets/images/p8.png";
+import img9 from "./assets/images/p1.png";
+import img10 from "./assets/images/p1.png";
+import img11 from "./assets/images/p1.png";
+
+
 const dataportfolio = [
     
     {
+        // glamor dekho
         your_img_url: profileImage1,
         description: "GLAMOR DEKHO - Experience Virtual Fashion Shows and Perfect Fits, Instantly.",
         link: "https://www.behance.net/gallery/214866829/GLAMOR-DEKHO",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        // hitwicket
+        your_img_url: img2,
+        description: "Re- Designing player's profile in the website to give better look",
+        link: "https://www.behance.net/gallery/214894315/Redesigning-players-profile",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        // travelling
+        your_img_url: img3,
+        description: "Web Travelling Partner- one stop solution to book all the things while you have a vations",
+        link: "https://www.behance.net/gallery/214988337/Web-Travelling-Partner",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        // vriddhi- stock market
+        your_img_url: img4,
+        description: "Vriddhi- a stock market prediction model",
+        link: "https://www.behance.net/gallery/215054287/Vriddhi-One-stop-solution-for-stock-market-trading",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        //kryptonics
+        your_img_url: img5,
+        description: "A Landing page's UI UX for WEB 3.0 company",
+        link: "https://www.behance.net/gallery/215108147/Kryptonics",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        // d-frame 
+        your_img_url: img6,
+        description: "The KERNING'S MANAGE- an app to manage the work load of projects",
+        link: "https://www.behance.net/gallery/215112515/The-Kernings-Manage",
     },
 
     {
+        // localville
         img: "https://picsum.photos/400/600/?grayscale",
         description: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        // vox vertex 
+        your_img_url: img8,
+        description: "TVOX VERTEX- an app to smoothen your event arrangements",
+        link: "https://www.behance.net/gallery/215059255/EVENT-MANAGEMENT-APP",
     },
     {
+        // deepfake detection model 
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "DEEPFAKE DETECTION -A website which will detect how much the image is made from AI",
+        link: "https://github.com/Santushti26/DeepFake-Detection",
     },
     {
+        // night time dehazing 
         img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Night time dehazing",
+        link: "https://github.com/Santushti26/image_enhancement_project",
     },
     {
+        // fire detection using ai ml 
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Fire Detection model using AI ML",
+        link: "https://github.com/Santushti26/Fire-detection-using-ML-iot-project-",
     },
     {
+        // weather recognition
         img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Weather recognition website",
+        link: "https://santushti26.github.io/Weatherrecog/",
     },
 ];
 
